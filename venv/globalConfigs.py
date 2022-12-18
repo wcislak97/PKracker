4a0a74ab7e58104ef2787e3ccc7823b69f55397a
wcislak97 <wcislak97@gmail.com>
2022-12-18 20:39:27 +0100

    message label changed, added custom path to globalConfigs.py

--- venv/globalConfigs.py
+++ venv/globalConfigs.py
@@ -1,8 +1,20 @@
 #paths for the app
 
 #zmieniajmy to tutaj pls żeby nie updejtować zawsze kodu
-appPath="./UI/app.ui"
-firstPagePath="./UI/first.ui"
-loginPath="./UI/login.ui"
-registrationPath="./UI/registration.ui"
+
+#odkomentuj sobie swoje i zakomentuj moje XD
+# sciezki Agaty
+
+# appPath="./UI/app.ui"
+# firstPagePath="./UI/first.ui"
+# loginPath="./UI/login.ui"
+# registrationPath="./UI/registration.ui"
+
+
+# sciezki Pauliny
+
+appPath="venv/UI/app.ui"
+firstPagePath="venv/UI/first.ui"
+loginPath="venv/UI/login.ui"
+registrationPath="venv/UI/registration.ui"
 
