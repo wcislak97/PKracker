255226da2199b3e41309dc05d4570a564b2a51de
napieralskaa <66205500+napieralskaa@users.noreply.github.com>
2023-01-03 22:29:51 +0100

    added spin and check box logic

--- venv/app.py
+++ venv/app.py
@@ -43,8 +43,36 @@ class App(QDialog):
         if(temp!='Słownikowa'):
             self.combo_slowniki.setCurrentIndex(-1)
             self.combo_slowniki.setDisabled(True)
+            self.checkBox.setDisabled(False)
+            self.checkBox_2.setDisabled(False)
+            self.checkBox_3.setDisabled(False)
+            self.checkBox_4.setDisabled(False)
+            self.spinBox.setDisabled(False)
+
         else:
             self.combo_slowniki.setDisabled(False)
+            self.checkBox.setDisabled(True)
+            self.checkBox_2.setDisabled(True)
+            self.checkBox_3.setDisabled(True)
+            self.checkBox_4.setDisabled(True)
+            self.spinBox.setDisabled(True)
+
+# or checkBox_2.isChecked() or checkBox_3.isChecked() or checkBox_4.isChecked():
+    def checkTheCheckBoxes(self):
+        lista=[0,0,0,0]
+        if self.checkBox.isChecked():
+            lista[0]=1
+        if self.checkBox_2.isChecked():
+            lista[2]=1
+        if self.checkBox_3.isChecked():
+            lista[1]=1
+        if self.checkBox_4.isChecked():
+            lista[3]=1
+
+        if lista==[0, 0, 0, 0]:
+            return 12345
+        else:
+            return lista
 
     def onDecodeButtonClicked(self):
         nazwaMetody = self.combo_metody.currentText()
@@ -64,10 +92,20 @@ class App(QDialog):
 
         elif nazwaMetody == "Brute force":
             if nazwaAlgorytmu == "MD5":
-                print("brute force md5 implementation")
+                value = self.checkTheCheckBoxes()
+                if value == 12345:
+                    self.lbl_wynik_out.setText('You have to mark at least one checkbox')
+                else:
+                    print(value)  #to jest lista, wartości od [0,0,0,0] -> do [1,1,1,1]
+                    valueSpinBox = self.spinBox.value()
+                    print(valueSpinBox)
+
+
+
+                    #implementacja md5 bruteforce
+
+                    print("brute force md5 implementation")
 
             else:
                 print("Some other algorithm brute force implementation")
 
-
-        self.lbl_wynik_out.setText('to zlamane haslo')
